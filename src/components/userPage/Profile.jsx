86e18d330a9da1e1ffb26024d7d01d509a399d15
samte9924 <tedsam04@gmail.com>
2024-10-10 18:25:01 +0200

    Restyled user profile card

--- src/components/userPage/Profile.jsx
+++ src/components/userPage/Profile.jsx
@@ -1,25 +1,46 @@
 import "../../styles/userPage/Profile.css";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 import { IoMdPeople } from "react-icons/io";
+import { FiActivity } from "react-icons/fi";
+import { GoRepo } from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
+import { SiGithub } from "react-icons/si";
 
 function Profile({ user, onClick, showRecentActivities }) {
   return (
     <div className="card">
       <div className="card-header">
-        <h1>{user.login}</h1>
+        <div>
+          <h1>{user.login}</h1>
+          <div className="links">
+            <a href={user.html_url} target="_blank" className="github-link">
+              <SiGithub size={32} />
+            </a>
+          </div>
+        </div>
         <img src={user.avatar_url} alt="avatar" />
       </div>
       <div className="card-body">
-        <h3>{user.bio}</h3>
-        <p>
-          <IoMdPeople />
-          <span>Followers: {user.followers}</span> |{" "}
-          <span>Following: {user.following}</span>
-        </p>
+        <p>{user.bio || "Empty bio."}</p>
+        <h3>
+          <IoMdPeople size={20} />
+          <span>
+            Followers: {user.followers} - Following: {user.following}
+          </span>
+        </h3>
         <div>
-          <span>Public repositories: {user.public_repos}</span>
-          <button onClick={onClick}>See more</button>
+          <h3>
+            <GoRepo size={20} />
+            <span>Public repositories: {user.public_repos}</span>
+          </h3>
+          <button onClick={onClick}>
+            <BsBoxArrowUpRight size={20} />
+          </button>
         </div>
-        <button onClick={showRecentActivities}>Attività recenti</button>
+        <button onClick={showRecentActivities} style={{ gap: "0.5rem" }}>
+          <FiActivity size={20} />
+          <span>Eventi recenti</span>
+        </button>
       </div>
     </div>
   );
